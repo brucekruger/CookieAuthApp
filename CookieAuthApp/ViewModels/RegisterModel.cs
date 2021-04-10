1975f6245d403b2bc0eaf34f7a7fdca2edc207e5
baranau_an <andrei_baranau1@epam.com>
2021-04-10 21:11:10 +0300

    Added project to repo.

---
+++ CookieAuthApp/ViewModels/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookieAuthApp.ViewModels
+{
+    public class RegisterModel : LoginModel
+    {
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Incorrect password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
